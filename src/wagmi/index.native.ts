874dcee36fbda4d05977ef938889a10ba8a01ec4
Samarth Saxena <samarthsaxena1672003@gmail.com>
2025-12-14 18:29:55 +0530

    fix variable names

--- src/wagmi/index.native.ts
+++ src/wagmi/index.native.ts
@@ -1,6 +1,6 @@
 import '../react-native/register.js'
 
 export * as Actions from './Actions.js'
-export { type PortoParameters, risewallet } from './Connector.js'
+export { type PortoParameters, riseWallet } from './Connector.js'
 export * as Hooks from './Hooks.native.js'
 export * as Query from './Query.js'
