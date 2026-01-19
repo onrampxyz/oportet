24f2db6fbc2f14d77ddf76db5e3c8e7b8b802ecc
pepper24bot <jen@riselabs.xyz>
2026-01-19 09:05:20 +0800

    remove InjectedSigner on signin option

--- apps/dialog/src/routes/-components/Email.tsx
+++ apps/dialog/src/routes/-components/Email.tsx
@@ -101,7 +101,7 @@ export function Email(props: Email.Props) {
         {actions.includes("sign-in") && (
           <div className="flex w-full flex-col gap-4 pt-2">
             <Button
-              className="flex-1! rounded-xl! p-2"
+              className="flex-1! rounded-xl p-2"
               data-testid="sign-in"
               disabled={status === "loading" || signingUp}
               icon={<IconScanFace className="size-5.25" />}
@@ -120,16 +120,6 @@ export function Email(props: Email.Props) {
                 ? "Sign in with Passkey"
                 : "Continue with Passkey"}
             </Button>
-            {!actions.includes("sign-up") && (
-              <InjectedSigner
-                disabled={status === "loading" || signingIn}
-                onSelect={(providerRdns) => {
-                  onApprove({ isInjected: true, providerRdns, signIn: true });
-                }}
-                providers={providers}
-                signingIn={status === "responding"}
-              />
-            )}
           </div>
         )}
 
